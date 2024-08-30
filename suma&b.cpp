9c0de36e305aae85e62aa21ee582cc2173ec62a9
Juanignaciofont713 <juanignaciofont713@gmail.com>
2024-08-30 20:32:10 -0300

    Add files via upload

---
+++ suma&b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b, suma;
+
+    cout << "Ingresa el primer numero: ";
+    cin >> a;
+    cout << "Ingresa el segundo numero: ";
+    cin >> b;
+
+    suma = a + b;
+
+    cout << "La suma de " << a << " y " << b << " es: " << suma << endl;
+
+    return 0;
+}
---
+++ suma&b.py
@@ -0,0 +1,9 @@
+# Suma de dos numeros
+
+def Suma(a, b):
+    return (a + b)
+
+a = float(input('Ingresar primer numero: '))
+b = float(input('Ingresar segundo numero: '))
+
+print(f'Suma de {a} y {b} es {Suma(a, b)}')
